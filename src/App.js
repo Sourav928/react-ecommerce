75df51974018d2ffb0b5b65fb95613af40841d13
Sourav928 <khandekarsourav8@gmail.com>
2025-05-10 11:57:11 +0530

    Added Login, Signup page along with routing.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Counter } from './features/counter/Counter';
 import Home from './pages/Home'
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:(
+     <Home></Home>
+    )
+  },
+  {
+    path:"/login",
+    element:<LoginPage></LoginPage>
+  },
+  {
+    path:"/signup",
+    element:<SignupPage></SignupPage>
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router}/>
     </div>
   );
 }
